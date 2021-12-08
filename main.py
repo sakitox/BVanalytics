1f99fa4c6ca4458c26dfbe38bd6c9ddf5f605798
sakitox <48063518+sakitox@users.noreply.github.com>
2021-12-08 12:59:14 +0000

    the script and the notes in jupyter

---
+++ main.py
@@ -0,0 +1,39 @@
+class SEOCausal(CausalImpact):
+    def __init__(self):
+        super(CausalImpact, self).__init__()
+    
+    def fit(self, int_time, start, end, 
+            test_set, dataset, alpha, 
+            events, seasons, btest, 
+            standardize):
+        
+        
+        
+        
+        
+        
+        
+        return 
+    
+    def transform(self, testset, dataset, metric,
+                  roll, min_test, min_data):
+        
+        
+        
+        
+        
+        
+        
+        return  
+    
+    def Rtransform(self, testset, dataset, metric,
+                  roll, nmarkets, min_test, min_data):
+        
+        
+        
+        
+        
+        
+        
+        
+        return
