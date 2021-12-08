070057b90590b243f25e35cc51390dd3a406bd75
sakitox <48063518+sakitox@users.noreply.github.com>
2021-12-08 13:22:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,9 @@ class SEOCausal(CausalImpact):
                 The column name for the target variable
             col: str
                 The column name for the markets, testset.col.name must match dataset.col.name case independent 
+            ranks: str
+                The column name that holds the ranking for the observations, defaults to date.
+                Accepts numerical and datetime ranking
             roll: int
                 The rolling window average. Used to smooth out data and, defaults at 2
             outlier: int
@@ -52,43 +55,73 @@ class SEOCausal(CausalImpact):
             WIP
         """
         
-        causal_input = dataset
+        def distance(testset=pd.DataFrame(), dataset=pd.DataFrame(), metric='str', ranks='date'
+              col='page', roll=2, outlier=1, min_test=320, min_data=300, metric2='sum'):
 
+    testset.columns = testset.columns.str.lower()
+    dataset.columns = dataset.columns.str.lower()
+    col = col.lower()
+    
+    assert testset[col].name == testset[col].name, 'Column names must match for the matched markets'
+    
+    causal_input = dataset
+    
+    test_set = causal_input[causal_input[col].isin(pdp[col])]
+    
+    for i in test_set[col]:
+        cutoff = test_set[test_set[col] == i].quantile(outlier)
+        temp = test_set[test_set[col] == i]
+        temp = temp[temp[metric] < cutoff[0]]
+        test_set_clean = pd.concat([test_set_clean, temp], ignore_index=True)
         test = causal_input[causal_input[col].isin(testset[col])]
+    
+    test = test_set_clean
+    causal_input = causal_input[~causal_input.page.isin(testcount.page)]
+    causal_input = pd.concat([causal_input, test_set_clean], ignore_index=True).sort_values(ranks)
+    
+    assert (test.iloc[1,:].all() != None)  & (test.iloc[0,:].all() != None), 'No markets match on test and data, check strings'
 
-        testcount = test.groupby([col]).count().sort_values(metric, ascending=False).reset_index()
+    testcount = test.groupby([col]).count().sort_values(metric, ascending=False).reset_index()
 
-        test_tops = testcount[testcount['date'] > min_test].reset_index()
+    assert testcount[ranks] > min_test, 'min_test must be lower than the maximum number of observations in testset'
+    
+    test_tops = testcount[testcount[danks] > min_test].reset_index()
 
-        causal_input.loc[causal_input[col].isin(test_tops[col]), col] = 'TEST'
+    causal_input.loc[causal_input[col].isin(test_tops[col]), col] = 'TEST'
+    
+    if metric2 = 'sum':
+        causal_input = causal_input.groupby([ranks, col]).sum().sort_values(metric, ascending=False).reset_index()
+    elif metric2 = 'mean':
+        causal_input = causal_input.groupby([ranks, col]).mean().sort_values(metric, ascending=False).reset_index()
+    elif raise ValueError('Supported aggregators are sum and mean')
+    
+    marketcount = causal_input.groupby([col]).count().sort_values(metric, ascending=False).reset_index()
+    
+    control_urls = marketcount[marketcount[ranks] > min_data].reset_index()
+    
+    assert marketcount[ranks] > min_data, 'min_data must be lower than the maximum number of observations in dataset'
+    
+    causal_control = causal_input.loc[causal_input[col].isin(control_urls[col]),]
+
+    pvt_table = causal_control.pivot_table(index=ranks, columns=col, values=metric, aggfunc=metric2).reset_index().fillna(0).set_index(ranks)
+
+    pvt_table = pvt_table.rolling(roll).mean()
+
+    pvt_table = pvt_table[roll-1:]
+
+    causal_control = pvt_table.melt(ignore_index=False).reset_index().sort_values(ranks).reset_index(drop=True)
+
+    markets = {}
+    for i in causal_control[[col]][col].unique():
+        markets[i] = causal_control[causal_control[col] == i].sort_values(ranks).reset_index(drop=True)[['value']]
+
+    distances = {}
+    for i in causal_control[[col]][col].unique():
+        distances[i] = dtw.dtw(markets['TEST'], markets[i]).distance
         
-        causal_input = causal_input.groupby(['date', col]).sum().sort_values(metric, ascending=False).reset_index()
-        
-        marketcount = causal_input.groupby([col]).count().sort_values(metric, ascending=False).reset_index()
-        
-        control_urls = marketcount[marketcount['date'] > min_data].reset_index() 
-        
-        causal_control = causal_input.loc[causal_input[col].isin(control_urls[col]),]
+    final = pd.DataFrame.from_dict(distances, orient='index', columns=['dist']).sort_values('dist')[1:].reset_index()
 
-        pvt_table = causal_control.pivot_table(index='date', columns=col, values=metric, aggfunc=metric2).reset_index().fillna(0).set_index('date')
-
-        pvt_table = pvt_table.rolling(roll).mean()
-
-        pvt_table = pvt_table[roll-1:]
-
-        causal_control = pvt_table.melt(ignore_index=False).reset_index().sort_values('date').reset_index(drop=True)
-
-        markets = {}
-        for i in causal_control[[col]][col].unique():
-            markets[i] = causal_control[causal_control[col] == i].sort_values('date').reset_index(drop=True)[['value']]
-
-        distances = {}
-        for i in causal_control[[col]][col].unique():
-            distances[i] = dtw.dtw(markets['TEST'], markets[i]).distance
-            
-        final = pd.DataFrame.from_dict(distances, orient='index', columns=['dist']).sort_values('dist')[1:].reset_index()
-
-        return  final
+    return  final
     
     def Rtransform(self, testset, dataset, metric,  #To do
                   roll, nmarkets, min_test, min_data):
