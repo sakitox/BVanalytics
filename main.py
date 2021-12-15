e2af740e8df06222daa3c6912f04280412b8636d
sakitox <48063518+sakitox@users.noreply.github.com>
2021-12-15 11:29:26 +0000

    update to functions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ class SEOCausal(CausalImpact):
         super(CausalImpact, self).__init__()
     
     def build(self, testset=pd.DataFrame(), dataset=pd.DataFrame(), metric='impressions', ranks='date',
-                col='page', roll=2, outlier=1, min_test=1, min_data=1, metric2='sum'):
+                col='page', begin_data = None, roll=2, outlier=1, min_test=1, min_data=1, metric2='sum'):
 
         """
         Builds a pd.DataFrame() by crossing two ranked -by dateime or else- unsorted and ungrouped datasets. 
@@ -16,10 +16,12 @@ class SEOCausal(CausalImpact):
                 The test set markets, testset.col.name must match dataset.col.name
             dataset: pd.DataFrame()
                 The global raw data with test and all control markets, can be ungrouped and unsorted.
-            metric: str
-                The column name for the target variable
+            metric: str, dict
+                The column name for the target variable. Accepts multi-variable input as a dictionary with key = column name and the value is the aggregation method (mean, sum, etc.)
             col: str
                 The column name for the markets, testset.col.name must match dataset.col.name case independent 
+            begin_data: datetime str
+                The start date for the dataset to be built
             ranks: str
                 The column name that holds the ranking for the observations, defaults to date.
                 Accepts numerical and datetime ranking
@@ -36,7 +38,7 @@ class SEOCausal(CausalImpact):
                 Minimum numbers of observations required from the overall market dataset to be added to model.
                 I.e., in a dataset with 20 markets and 365 daily observations we would recommend (325-360) 
             metric2 = str
-                Function by which to aggregate the data.
+                Function by which to aggregate the data. Not used if metric input is a dictionary.
                 Examples: sum if impressions, sessions, etc. and mean if position, users, etc.
                 
         Returns
@@ -54,85 +56,87 @@ class SEOCausal(CausalImpact):
         
         assert testset[col].name == testset[col].name, 'Column names must match for the matched markets'
         
-        if metric2 == 'sum':
-            causal_input = dataset.groupby([ranks, col]).sum().sort_values(ranks, ascending=False).reset_index()
-        elif metric2 == 'mean':
-            causal_input = dataset.groupby([ranks, col]).mean().sort_values(ranks, ascending=False).reset_index()
+        if type(metric) == str:
+            metric_dict = {metric : metric2}
+        elif type(metric) == dict:
+            metric_dict = metric
         else:
-            raise ValueError('Supported aggregators are sum and mean')
+            raise ValueError('Metric must be a string or a dictionary')
+
+        
+        causal_input = dataset.groupby([ranks, col]).agg(metric_dict).sort_values(ranks).reset_index()
 
         test_set = causal_input[causal_input[col].isin(testset[col])]
         
         assert (test_set.iloc[1,:].all() != None)  & (test_set.iloc[0,:].all() != None), 'No markets match on test and data, check strings'
 
-        testcount = test_set.groupby([col]).count().sort_values(metric, ascending=False).reset_index()
+        testcount = test_set.groupby([col]).count().sort_values(ranks, ascending=False).reset_index()
         
-        assert testcount[metric].max() >= min_test, 'min_test must be lower than the maximum number of observations in testset'
+        assert testcount[ranks].max() >= min_test, 'min_test must be lower than the maximum number of observations in testset'
         
-        test_tops = testcount[testcount[ranks] >= min_test].reset_index()
+        test_tops = testcount[testcount[ranks] >= min_test].reset_index() #to be expanded, same with row 106
         
-        if metric2 == 'sum':
-            test_set =  causal_input[causal_input[col].isin(test_tops[col])]
-            test_set = test_set.groupby([ranks, col]).sum().sort_values(ranks, ascending=False).reset_index()
-        elif metric2 == 'mean':
-            test_set =  causal_input[causal_input[col].isin(test_tops[col])]
-            test_set = test_set.groupby([ranks, col]).mean().sort_values(ranks, ascending=False).reset_index()
+        test_set =  causal_input[causal_input[col].isin(test_tops[col])]
+        test_set = test_set.groupby([ranks, col]).agg(metric_dict).sort_values(ranks).reset_index()
             
         test_set_clean = pd.DataFrame()
+        
         if outlier < 1:
             for i in test_tops[col].unique():
                 cutoff = test_set[test_set[col] == i].quantile(outlier)
                 temp = test_set[test_set[col] == i]
-                temp = temp[temp[metric] < cutoff[0]]
-                test_set_clean = pd.concat([test_set_clean, temp], ignore_index=True)
+                for key, value in metric_dict.items():
+                    temp = temp[temp[key] < cutoff.loc[key]]
+                    test_set_clean = pd.concat([test_set_clean, temp], ignore_index=True)
             test_set = test_set_clean
-
             causal_input = causal_input[~causal_input[col].isin(testcount[col])]
-            causal_input = pd.concat([causal_input, test_set], ignore_index=True).sort_values(ranks)
+            causal_input = pd.concat([causal_input, test_set], ignore_index=True).sort_values(ranks).reset_index(drop=True)
         
         test_items = causal_input[causal_input[col].isin(test_tops[col])]
         
         causal_input.loc[causal_input[col].isin(test_tops[col]), col] = 'TEST'
         
-        if metric2 == 'sum':
-            causal_input = causal_input.groupby([ranks, col]).sum().sort_values(ranks, ascending=True).reset_index()
-        elif metric2 == 'mean':
-            causal_input = causal_input.groupby([ranks, col]).mean().sort_values(ranks, ascending=True).reset_index()
-        else:
-            raise ValueError('Supported aggregators are sum and mean')
+        causal_input = causal_input.groupby([ranks, col]).agg(metric_dict).sort_values(ranks).reset_index()
         
-        marketcount = causal_input.groupby([col]).count().sort_values(metric, ascending=False).reset_index()
+        marketcount = causal_input.groupby([col]).count().sort_values(ranks, ascending=False).reset_index()
 
-        if marketcount[marketcount[col] == 'TEST'][metric].max() <= min_data:
-            min_data = marketcount[marketcount[col] == 'TEST'][metric].max()
+        if marketcount[marketcount[col] == 'TEST'][ranks].max() <= min_data:
+            min_data = marketcount[marketcount[col] == 'TEST'][ranks].max()
         
-        assert marketcount[metric].max() >= min_data, 'min_data must be lower than the maximum number of observations in dataset'    
+        assert marketcount[ranks].max() >= min_data, 'min_data must be lower than the maximum number of observations in dataset'    
         assert min_test <= min_data, 'Test observations have to be equal or higher than cutoff point'
         
-        control_urls = marketcount[marketcount[ranks] >= min_data].reset_index()
+        control_urls = marketcount[marketcount[ranks] >= min_data].reset_index() #to be expanded, same with row 75
 
         causal_control = causal_input.loc[causal_input[col].isin(control_urls[col]),]
 
-        pvt_table = causal_control.pivot_table(index=ranks, columns=col, values=metric, aggfunc=metric2).reset_index().fillna(0).set_index(ranks)
+        pvt_table = causal_control.pivot_table(index=ranks, columns=col, aggfunc=metric_dict).reset_index().fillna(0).set_index(ranks)
 
         if roll > 1:
             pvt_table = pvt_table.rolling(roll).mean()
             pvt_table = pvt_table[roll-1:]
 
-        causal_control = pvt_table.melt(ignore_index=False).reset_index().sort_values(ranks).reset_index(drop=True)
-        
-        causal_control
+        causal_control = pvt_table.melt(ignore_index=False).reset_index().sort_values(ranks).reset_index(drop=True).rename(columns = {None : "signal"})
+
+        causal_control = causal_control.pivot_table(index=[ranks,col], columns='signal').reset_index(level=1).fillna(0)
+
+        causal_control.columns = ["_".join(a) for a in causal_control.columns.to_flat_index()]
+
+        if begin_data != None:
+            causal_control = causal_control[causal_control[ranks] >= begin_data]
         
         return causal_control
 
     
-    def distance(self, causal_control, col='page', ranks='date',scaling=True):
+    def distance(self, causal_control, end_date,  col='page', ranks='date',scaling=True):
 
         """
         Builds a pd.DataFrame() by finding all the dynamic time warped distances of each internal market against the 'TEST' set generated using build(). 
         ----
             causal_control: pd.DataFrame()
                 The dataset built using build()
+            end_date: datetime str
+                The end date to match the markets, the intervention day.
             col: str
                 The column name for the markets where 'TEST' is found.
             ranks: str
@@ -151,6 +155,8 @@ class SEOCausal(CausalImpact):
             WIP
         """
 
+        causal_control = causal_control[causal_control[ranks] < end_date]
+        
         markets = {}
         for i in causal_control[col].unique():
             markets[i] = causal_control[causal_control[col] == i].sort_values(ranks).reset_index(drop=True)[['value']]
